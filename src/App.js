16e3f5409d962b9abacd206825a710df33f8d034
Sujan Thapa <sujan.st98644@gmail.com>
2024-05-24 22:56:19 +0545

    added servives-part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Services from "./components/Services";
 
 function App() {
   return (
     <div className={`min-h-screen flex flex-col justify-center bg-[#CDE8E5]` }>
       <Header/>
+      <Services/>
     </div>
   );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const Services = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
+  return (
+    <main className="my-5 text-white font-sora font-semibold text-2xl">
+      <h1 className="font-sora text-3xl font-semibold mb-4 text-black">SERVICES</h1>
+      <Carousel responsive={responsive} infinite={true} autoPlaySpeed={1000}>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/design.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">Web Design</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/design.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">Database</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/typer.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">Deployment</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/stars.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">API</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/design.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">SEO</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/design.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">Data Entry</p>
+        </div>
+        <div
+          className="bg-[#4D869C] my-3 rounded w-52 h-72 shadow-xl bg-cover"
+          style={{ backgroundImage: "url('/design.jpg')" }}
+        >
+          <p className="ml-3 mt-5 shadow-xl">Logo</p>
+        </div>
+      </Carousel>
+    </main>
+  );
+};
+
+export default Services;
