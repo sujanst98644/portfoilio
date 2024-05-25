06dae16a4f7d43656a2b72839db5698ea40f89fc
Sujan Thapa <sujan.st98644@gmail.com>
2024-05-25 08:42:36 +0545

    added Project section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Services from "./components/Services";
 import Skills from "./components/skills";
+import Projects from "./components/projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Services/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
